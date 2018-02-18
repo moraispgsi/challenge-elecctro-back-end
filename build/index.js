bdeae083302aa8b04896fb998c58ae15bd84a92d
moraispgsi <moraispgsi@gmail.com>
2018-02-18 22:51:24 +0000

    Minor

--- build/index.js
+++ build/index.js
@@ -711,11 +711,12 @@ var routeLogin = {
         return reply('Authentication failed due to: ' + request.auth.error.message);
       }
 
+      console.log('registration');
       // Perform any account lookup or registration, setup local session,
       // and redirect to the application. The third-party credentials are
       // stored in request.auth.credentials. Any query parameters from
       // the initial request are passed back via request.auth.credentials.query.
-      return reply.redirect('/home');
+      return reply.redirect('/');
     }
   }
 };
--- src/index.js
+++ src/index.js
@@ -236,12 +236,45 @@ const routeRemoveTodo = {
   }
 };
 
+const routeLogin = {
+  method: ['GET', 'POST'], // Must handle both GET and POST
+  path: '/login',          // The callback endpoint registered with the provider
+  config: {
+    auth: 'google',
+    handler: function (request, reply) {
+
+      if (!request.auth.isAuthenticated) {
+        return reply('Authentication failed due to: ' + request.auth.error.message);
+      }
+
+      console.log('registration');
+      // Perform any account lookup or registration, setup local session,
+      // and redirect to the application. The third-party credentials are
+      // stored in request.auth.credentials. Any query parameters from
+      // the initial request are passed back via request.auth.credentials.query.
+      return reply.redirect('/');
+    }
+  }
+};
+
+const routeLogout = {
+  method: 'GET',
+  path: '/logout',
+  options: {
+    handler: (request, h) => {
+      request.server.app.cache.drop(request.state['sid-example'].sid);
+      request.cookieAuth.clear();
+      return 'logged out';
+    }
+  }
+};
+
 const routeIndex = {
   method: 'GET',
   path: '/',
   config: {
     auth: {
-      strategy: 'google', //authorisation is of 'hapi-auth-cookie' type
+      strategy: 'session', //authorisation is of 'hapi-auth-cookie' type
       mode: 'try' //allows you to proceed to a path handler even if not authenticated
     },
     handler: function (request, h) {
@@ -285,6 +318,8 @@ async function startServer() {
     routeUpdateTodo,
     routeGetTodos,
     routeRemoveTodo,
+    routeLogin,
+    routeLogout,
     routeIndex
   ]);
 
